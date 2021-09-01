b8b02b12a2acc06dbbc4f5c874d8de4ff2ba39f8
RedPlus <redplus@redplus.net>
2021-09-02 05:41:11 +0900

    문자열을 숫자로 변환하기

---
+++ Challenge/ascii_to_number.c
@@ -0,0 +1,18 @@
+//[?] 문자열을 숫자로 변환하기 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char* str = "3.141592";
+
+    int i = atoi(str);      // 문자열을 int 형식으로 변환
+    long l = atol(str);     // 문자열을 long 형식으로 변환
+    float f = atof(str);    // 문자열을 float 형식으로 변환
+
+    printf("%d\n", i); // 3
+    printf("%ld\n", l); // 3
+    printf("%.7f\n", f); // 3.1415920
+
+    return 0;
+}
