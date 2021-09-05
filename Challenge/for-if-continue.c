1de663ce7f18daf80faf840d2e171a6781546f7b
RedPlus <redplus@redplus.net>
2021-09-05 15:15:59 +0900

    continue 문으로 코드 건너뛰기

---
+++ Challenge/for-if-continue.c
@@ -0,0 +1,16 @@
+//[?] continue 문을 사용하여 반복문(for, while, do)에서 다음 반복으로 이동하기
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        if (i % 2 == 0)
+        {
+            //[!] 현재 반복 중지 후 다음 반복으로 이동
+            continue; // 짝수 건너뛰기
+        }
+        printf("%d\n", i);
+    }
+    return 0;
+}
