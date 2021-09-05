4717a56ab249e0e1fd91d31beccf8684d75359f6
RedPlus <redplus@redplus.net>
2021-09-05 11:20:27 +0900

    1부터 10까지 정수의 합을 구해나갈 때 합이 22 이상이 되면 멈추는 프로그램

---
+++ Challenge/while-sum-break.c
@@ -0,0 +1,26 @@
+// 1부터 10까지 정수의 합을 구해나갈 때 합이 22 이상이 되면 멈추는 프로그램
+#include <stdio.h>
+
+int main(void)
+{
+    int goal = 22;
+    int sum = 0;
+
+    int i = 1;
+    while (i <= 10)
+    {
+        sum += i;
+
+        if (sum >= goal)
+        {
+            break; // 반복문 탈출
+        }
+
+        i++;
+    }
+
+    printf("1부터 %d까지의 합은 %d이고, ", i, sum);
+    printf("목표치 %d이상을 달성했습니다.\n", goal);
+
+    return 0;
+}
