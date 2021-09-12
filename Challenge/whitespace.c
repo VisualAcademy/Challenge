d87e5789af365778294b2303e7e890925990ab30
RedPlus <redplus@redplus.net>
2021-09-13 06:53:16 +0900

    공백 처리

---
+++ Challenge/whitespace.c
@@ -0,0 +1,12 @@
+// 공백(Whitespace): 프로그래밍 언어에서 공백은 무시된다. 
+#include <stdio.h>
+
+int main(void)
+{
+    printf("C 언어\n");
+    printf  (  "C 언어\n"  ) ;
+    printf  (  
+        "C 언어\n"  
+    ) ;
+    return 0;
+}
