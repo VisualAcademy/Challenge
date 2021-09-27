d41273f7b8df67cd144ca0f46bfbeeee95e3be74
RedPlus <redplus@redplus.net>
2021-09-27 14:40:21 +0900

    Code Cleanup

--- Challenge/sring_reverse_english.c
+++ Challenge/sring_reverse_english.c
@@ -7,14 +7,15 @@ int main(void)
 {
     char source[80] = "";
     int r, i, j;
-    char temp; 
+    char temp;
 
-    printf("입력: "); r = scanf("%s", source); 
+    printf("입력: "); r = scanf("%s", source);
 
     //[!] 처음 문자와 마지막 문자를 계속해서 교환하고 증감
     for (i = 0, j = strlen(source) - 1; i < j; i++, j--)
     {
-        temp = source[i]; source[i] = source[j]; source[j] = temp; // 교환 
+        // 바꾸기(SWAP) 
+        temp = source[i]; source[i] = source[j]; source[j] = temp;
     }
 
     printf("출력: %s\n", source);
