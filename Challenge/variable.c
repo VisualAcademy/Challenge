927b2332f2306a3a3e773b0ebd19c800ecdbef31
RedPlus <redplus@redplus.net>
2021-09-30 15:54:05 +0900

    variable.c

---
+++ Challenge/variable.c
@@ -0,0 +1,10 @@
+// 변수(Variable)
+#include <stdio.h>
+
+int main(void)
+{
+    int number; //[1] 정수형 변수 선언하기
+    number = 7; //[2] 변수에 값 대입(할당)하기
+    printf("%d\n", number); //[3] 변수의 값을 화면에 출력하기
+    return 0;
+}
