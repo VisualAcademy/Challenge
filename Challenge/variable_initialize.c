906591b615a12b5007b0a50b08f9f7324352d430
RedPlus <redplus@redplus.net>
2021-10-02 21:11:13 +0900

    콤마 기준으로 여러 개의 변수 동시 생성

---
+++ Challenge/variable_initialize.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //[1] 변수 선언과 초기화를 한 번에 처리 
+    int first = 10;
+
+    //[2] 콤마를 사용하여 여러 개의 변수를 동시에 선언 및 초기화
+    int second = 20, third = 30;
+
+    printf("%d, %d, %d\n", first, second, third);
+
+    return 0;
+}
