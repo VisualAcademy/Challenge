1dffd09330b8dcb5bd523c441d0bfcff47bd982e
Park YongJune <redplus@redplus.net>
2021-10-06 08:53:53 +0900

    float_demo.c

---
+++ Challenge/float_demo.c
@@ -0,0 +1,10 @@
+//[?] float 키워드: 실수형 데이터 형식(32비트 부동 소수점 숫자)
+#include <stdio.h>
+
+int main(void)
+{
+    float f = 99.99f; // 단정밀도 부동 소수점 변수를 선언하고 값을 할당 
+    printf("%.2f\n", f); // 99.99
+
+    return 0;
+}
