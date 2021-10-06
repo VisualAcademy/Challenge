9fb62977d2989ace2661f681c3ea82b20b6ff10c
Park YongJune <redplus@redplus.net>
2021-10-07 03:38:41 +0900

    문자열 데이터 형식(char*)

---
+++ Challenge/string_demo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char* message = "æ»≥Á«œººø‰.";
+    printf("%s\n", message);
+
+    message = "π›∞©Ω¿¥œ¥Ÿ.";
+    printf("%s\n", message);
+
+    return 0;
+}
