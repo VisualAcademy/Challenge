9e2ff77a457c1c85f7c40455d16cc6fe4c6665d5
Park YongJune <redplus@redplus.net>
2021-10-07 05:09:13 +0900

    심사문제: 변수를 선언하고 값 할당하기

---
+++ Challenge/judge_variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number1 = 0;;
+
+    number1 = 100;
+    int number2 = 200, number3 = 300;
+
+    printf("%d %d %d\n", number1, number2, number3);
+
+    return 0;
+}
