4bba9d4f5bef0ba239b3c361ce87e6cfe369cb51
Park YongJune <redplus@redplus.net>
2021-10-08 14:35:04 +0900

    Code Cleanup

---
+++ Challenge/operator_description.c
@@ -0,0 +1,15 @@
+//[?] 연산자(Operator): 더하기, 빼기와 같은 연산을 진행하는 키워드
+#include <stdio.h>
+
+int main(void)
+{
+    //[?] 식(Expression)
+    printf("%d\n", 3 + 5); // 8
+    printf("%d\n", 3 - 5); // -2
+
+    //[?] 문(Statement) 
+    printf("%d\n", 3 * 5); // 15
+    printf("%d\n", 3 / 5); // 0
+
+    return 0;
+}
