a4b4ca1c68ad4773bef382201292fce626532ddd
Park YongJune <redplus@redplus.net>
2021-10-13 18:31:58 +0900

    논리 자료형 사용하기

---
+++ Challenge/boolean_demo.c
@@ -0,0 +1,14 @@
+//[?] 논리 자료형: 참(True) 또는 거짓(False) 값 저장
+#include <stdio.h>
+#include <stdbool.h> // bool, true(1), false(0)
+
+int main(void)
+{
+    bool bln = true; // 참(1)
+    printf("%d\n", bln); // 1
+
+    bool isFalse = false; // 거짓(0)
+    printf("%d\n", isFalse); // 0
+
+    return 0;
+}
