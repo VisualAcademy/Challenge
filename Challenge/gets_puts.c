99a4d0af8d6abc70bce11470b0104dc95a75dadc
Park YongJune <redplus@redplus.net>
2021-10-14 11:28:53 +0900

    gets(), puts()

---
+++ Challenge/gets_puts.c
@@ -0,0 +1,15 @@
+//[?] 한 줄의 문자열을 입력받고 출력하는 함수: gets(), puts() 
+#include <stdio.h>
+
+int main(void)
+{
+    char buffer[80] = { NULL };
+
+    //[1] 표준 입력으로부터 한 줄 입력받기: gets()
+    gets(buffer); 
+
+    //[2] 표준 출력으로 문자열 출력: puts()
+    puts(buffer); 
+
+    return 0;
+}
