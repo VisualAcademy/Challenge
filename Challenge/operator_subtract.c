75e858aff996481dfc325e16cd2395f335c844d8
Park YongJune <redplus@redplus.net>
2021-10-16 23:48:32 +0900

    operator_subtract.c

---
+++ Challenge/operator_subtract.c
@@ -0,0 +1,13 @@
+//[?] 빼기(-) 연산자
+#include <stdio.h>
+
+int main(void)
+{
+    float i = 3.14f;
+    float j = 3.14f;
+    float k = i - j; // k = 3.14 - 3.14
+
+    printf("%.2f\n", k); // 0.00
+
+    return 0;
+}
