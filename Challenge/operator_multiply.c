cd527088790e2f00ba1b2b8f6567b53f506ff726
Park YongJune <redplus@redplus.net>
2021-10-16 23:50:32 +0900

    Code Cleanup

---
+++ Challenge/operator_multiply.c
@@ -0,0 +1,13 @@
+//[?] 곱하기(*) 연산자
+#include <stdio.h>
+
+int main(void)
+{
+    long i = 100;
+    long j = 200;
+    long k = i * j; // k = 100 * 200
+
+    printf("%ld\n", k); // 20000
+
+    return 0;
+}
