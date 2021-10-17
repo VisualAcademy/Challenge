eec97497a9c24a966217f16fe5b24507f10dff0e
Park YongJune <redplus@redplus.net>
2021-10-18 03:26:54 +0900

    Code Cleanup

---
+++ Challenge/switch_weather.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+int main(void)
+{
+    int weather = 0;
+    int r = 0;
+    printf("¿À´Ã ³¯¾¾´Â ¾î¶²°¡¿ä? (¸¼À½(1), Èå¸²(2), ºñ(3), ´«(4), ...)\n");
+
+    r = scanf("%d", &weather);
+
+    switch (weather)
+    {
+    case 1:
+        printf("¿À´Ã ³¯¾¾´Â ¸¼±º¿ä.");
+        break;
+    case 2:
+        printf("¿À´Ã ³¯¾¾´Â Èå¸®±º¿ä.");
+        break;
+    case 3:
+        printf("¿À´Ã ³¯¾¾´Â ºñ°¡ ¿À´Â±º¿ä.");
+        break;
+    default:
+        printf("È¤½Ã ´«ÀÌ ³»¸®³ª¿ä?");
+        break;
+    }
+
+    return 0;
+}
