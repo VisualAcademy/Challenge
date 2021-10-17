53f7714669d612df329535a4c98eca127fbb04db
Park YongJune <redplus@redplus.net>
2021-10-18 02:59:55 +0900

    Code Cleanup

---
+++ Challenge/switch_expression.c
@@ -0,0 +1,22 @@
+// switch 문 사용하기
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 2;
+
+    switch (x)
+    {
+    case 1:
+        printf("1입니다.\n"); // x가 1일 때 실행
+        break;
+    case 2:
+        printf("2입니다.\n"); // x가 2일 때 실행
+        break;
+    default:
+        printf("1과 2가 아닙니다.\n"); // 기본값
+        break;
+    }
+
+    return 0;
+}
