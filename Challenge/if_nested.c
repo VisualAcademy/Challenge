6c33996cff9677916056842e173ad9f5edc10e98
Park YongJune <redplus@redplus.net>
2021-10-19 19:33:59 +0900

    중첩 된 if 문

---
+++ Challenge/if_nested.c
@@ -0,0 +1,18 @@
+// ÁßÃ¸ µÈ if ¹®
+#include <stdio.h>
+
+int main(void)
+{
+    char name = 'C';
+    int version = 22;
+
+    if (name == 'C')
+    {
+        if (version == 22)
+        {
+            printf("%c %d\n", name, version);
+        }
+    }
+
+    return 0;
+}
