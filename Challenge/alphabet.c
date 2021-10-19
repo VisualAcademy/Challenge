f5cde976cfe4b3a4da0478925e5e9619cb0a454d
Park YongJune <redplus@redplus.net>
2021-10-20 04:04:24 +0900

    알파벳 문자 비교 판단

---
+++ Challenge/alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char c;
+    printf("알파벳 한 자를 입력하세요.\n");
+    c = getchar(); // 사용자로부터 문자 하나 입력
+
+    if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+    {
+        printf("%c는 알파벳 문자입니다.\n", c);
+    }
+
+    return 0;
+}
