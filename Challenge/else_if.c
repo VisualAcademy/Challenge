334a5468f89fff88852a7886f062e92e690430d8
Park YongJune <redplus@redplus.net>
2021-10-20 02:53:44 +0900

    else if 문을 사용한 성적 결과를 나타내는 프로그램

---
+++ Challenge/else_if.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int score = 59;
+
+    if (score >= 90)
+    {
+        printf("A\n");
+    }
+    else if (score >= 80)
+    {
+        printf("B\n");
+    }
+    else if (score >= 70)
+    {
+        printf("C\n");
+    }
+    else if (score >= 60)
+    {
+        printf("D\n");
+    }
+    else
+    {
+        printf("F\n");
+    }
+
+    return 0;
+}
