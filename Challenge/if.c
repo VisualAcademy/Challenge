c8af5a4240f510b595f1fd4c123fb0ef92a00d72
Park YongJune <redplus@redplus.net>
2021-10-19 18:19:59 +0900

    if 문을 사용하여 하나의 조건 처리

---
+++ Challenge/if.c
@@ -0,0 +1,15 @@
+// if 문을 사용하여 하나의 조건 처리 
+#include <stdio.h>
+
+int main(void)
+{
+    int score = 60;
+
+    // score가 60 이상이면 "합격" 출력
+    if (score >= 60)
+    {
+        printf("\"합격\"\n");
+    }
+
+    return 0;
+}
