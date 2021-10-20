5bb9008ef66abd87b9e9690f8aaac4dad5c196c6
Park YongJune <redplus@redplus.net>
2021-10-21 05:48:29 +0900

    관계형 연산자

---
+++ Challenge/relational_operator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // 3이 5보다 작은지?
+    printf("%d\n", 3 < 5); // 1 -> True
+    // 3이 5보다 큰지? 
+    printf("%d\n", 3 > 5); // 0 -> False
+
+    return 0;
+}
