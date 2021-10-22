e11b1673286c68998499edffff913de5c3e22746
Park YongJune <redplus@redplus.net>
2021-10-22 09:40:39 +0900

    관계형 연산자 사용하기

---
+++ Challenge/operator_relational.c
@@ -0,0 +1,18 @@
+// 관계형 연산자(비교 연산자;Relational Operator, Comparative Operator)
+// 관계형 연산자: < 작음, <= 작거나 같음, > 큼, >= 크거나 같음, == 같음, != 다름
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 3;
+    int y = 5;
+
+    printf("%d\n", x == y); // false(0)
+    printf("%d\n", x != y); // true(1)
+    printf("%d\n", x > y); // 0
+    printf("%d\n", x >= y); // 0
+    printf("%d\n", x < y); // 1
+    printf("%d\n", x <= y); // 1
+
+    return 0;
+}
