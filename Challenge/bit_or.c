695f5d66b5f00835e96d1f447f60e69dc543b285
Park YongJune <redplus@redplus.net>
2021-10-22 09:47:41 +0900

    비트 OR 연산자: | 연산자

---
+++ Challenge/bit_or.c
@@ -0,0 +1,21 @@
+// | 연산자: 비트 OR 연산자(하나라도 1이면 1)
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 0b1010; // 10
+    int y = 0b1100; // 12
+
+    // x를 이진수로 표현 -> 십진수로 표현
+    printf("%d\n", x); // 10
+
+    // y를 이진수로 표현 -> 십진수로 표현
+    printf("%d\n", y); // 12
+
+    printf("--\n");
+
+    // x OR y를 십진수로 표현 
+    printf("%d\n", x | y); // 14
+
+    return 0;
+}
