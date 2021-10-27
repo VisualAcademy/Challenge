e7b9dc9193a8d531ed817a873e0af35382e2875c
Park YongJune <redplus@redplus.net>
2021-10-27 10:28:32 +0900

    for 루프를 사용하여 1부터 5까지 출력하기

---
+++ Challenge/for.c
@@ -0,0 +1,15 @@
+//[?] for 루프를 사용하여 1부터 5까지 출력하기
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 0;
+
+    // 1부터 5까지 1씩 증가 => 5번 반복
+    for (i = 1; i <= 5; i++)
+    {
+        printf("Count: %d\n", i); // 1부터 5까지 출력
+    }
+
+    return 0;
+}
