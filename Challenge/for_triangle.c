234f8b81e3be88fd06482bb5f0420a2b8809bbc1
Park YongJune <redplus@redplus.net>
2021-10-29 09:23:50 +0900

    for 문을 사용하여 별표 기호로 삼각형 만들기

---
+++ Challenge/for_triangle.c
@@ -0,0 +1,18 @@
+//[?] for 문을 사용하여 별표 기호로 삼각형 만들기
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j;
+
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++) // 행 번호에 맞게 별표 출력(1부터 5까지) 
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
