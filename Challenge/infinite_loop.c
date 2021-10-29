32dc096d039260e9293e782f2f36007d9a73fe1c
Park YongJune <redplus@redplus.net>
2021-10-29 09:39:49 +0900

    프로그램이 계속 실행되는 무한 루프 만들기

---
+++ Challenge/infinite_loop.c
@@ -0,0 +1,13 @@
+//[?] 무한 루프 만들기 
+#include <stdio.h>
+
+int main(void)
+{
+    // 무한 루프
+    for ( ; ; ) // for 문에 아무런 코드를 표현하지 않으면 무한 루프 코드로 실행
+    {
+        printf("무한 루프\n");
+    }
+
+    return 0;
+}
