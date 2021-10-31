1f81337c5732db8fbe7dffb2cc28408b2f09da44
Park YongJune <redplus@redplus.net>
2021-10-31 17:22:06 +0900

    비트 XOR 연산자

---
+++ Challenge/bit_xor.c
@@ -0,0 +1,21 @@
+// ^ 연산자: 비트 XOR 연산자(서로 다르면 1)
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 0b1010; // 10
+    int y = 0b1100; // 12
+
+    // x를 십진수로 표현
+    printf("%d\n", x); // 10
+
+    // y를 십진수로 표현
+    printf("%d\n", y); // 12
+
+    printf("--\n");
+
+    // x XOR y를 십진수로 표현 
+    printf("%2d\n", x ^ y); // 6
+
+    return 0;
+}
