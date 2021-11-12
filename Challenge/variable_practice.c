a361b49802995279278fb97cb96846a10d7eea84
Park YongJune <redplus@redplus.net>
2021-11-12 09:58:21 +0900

    여러 개의 변수를 선언과 동시에 초기화하기

---
+++ Challenge/variable_practice.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // 여러 개의 변수를 선언과 동시에 초기화
+    int first = 10, second = 20, third = 30;
+
+    printf("%d, %d, %d\n", first, second, third);
+
+    return 0;
+}
