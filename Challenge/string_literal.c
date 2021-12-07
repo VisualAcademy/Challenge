39c106b9b2924eeeb081c933c6ed49c44dd8fac3
Park YongJune <redplus@redplus.net>
2021-12-08 05:48:49 +0900

    문자열 리터럴

---
+++ Challenge/string_literal.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    // 문자열 리터럴
+    // C 언어에서 문자열은 큰 따옴표로 묶음
+    // "C Language", "Hello, World"
+    printf("%s, %s\n", "C Language", "Hello, World!"); 
+}
