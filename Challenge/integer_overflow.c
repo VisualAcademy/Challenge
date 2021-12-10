f8e51e464fd6463dd6b1c72145597e202d0016f0
Park YongJune <redplus@redplus.net>
2021-12-11 04:42:17 +0900

    Code Cleanup

--- Challenge/integer_overflow.c
+++ Challenge/integer_overflow.c
@@ -8,8 +8,8 @@ int main(void)
 {
     // int 형식의 범위: -2147483648 ~ +2147483647
     // 담을 수 있는 범위를 벗어남: 오버플로(넘침) 발생
-    int num = (2147483647 + 2147483647); // INT_MAX + INT_MAX
-    printf("%d\n", num); // 잘못된 데이터 값 출력
+    //int num = (2147483647 + 2147483647); // INT_MAX + INT_MAX
+    //printf("%d\n", num); // 잘못된 데이터 값 출력
 
     return 0; 
 }
