13381b6a4688fdbb027fa9c5c40b42fe1b2642d9
Park YongJune <redplus@redplus.net>
2021-12-11 04:34:17 +0900

    C 언어_integer-overflow_산술 오버플로 발생시키기

---
+++ Challenge/integer_overflow.c
@@ -0,0 +1,15 @@
+// integer_overflow.c
+#include <stdio.h>
+
+// limits.h 헤더 파일의 INT_MAX, INT_MIN으로 정수 형식의 최댓값, 최솟값 알 수 있음
+// #include <limits.h>
+
+int main(void)
+{
+    // int 형식의 범위: -2147483648 ~ +2147483647
+    // 담을 수 있는 범위를 벗어남: 오버플로(넘침) 발생
+    int num = (2147483647 + 2147483647); // INT_MAX + INT_MAX
+    printf("%d\n", num); // 잘못된 데이터 값 출력
+
+    return 0; 
+}
