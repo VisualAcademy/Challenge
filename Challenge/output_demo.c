da4ab99bab04afb542285dee8733e97a05fd8927
Park YongJune <redplus@redplus.net>
2021-12-13 08:25:07 +0900

    Code Cleanup

--- Challenge/output_demo.c
+++ Challenge/output_demo.c
@@ -2,10 +2,10 @@
 
 int main(void)
 {
-    printf("%d\n", 1234);
-    putchar('A');
-    putchar('\n');
-    puts("C Language");
+    printf("%d\n", 1234); // 서식에 맞게 출력
+    putchar('A'); // 문자 하나 출력
+    putchar('\n'); // 이스케이프 시퀀스는 문자 하나로 취급
+    puts("C Language"); // 문자열 출력
 
     return 0;
 }
