ff7e9e9a0380fc0bb47594faaa022784f10851cb
Park YongJune <redplus@redplus.net>
2021-12-12 14:08:21 +0900

    char_array_string

--- Challenge/_.c
+++ Challenge/_.c
@@ -1,7 +1,19 @@
+// char_array_string.c
 #include <stdio.h>
 
 int main(void)
 {
+    char hello[80]; // 문자열 저장 공간 생성
+    char world[40] = "World"; // 문자 배열 선언과 동시에 초기화 
 
-	return 0;
+    hello[0] = 'H'; // 문자 하나씩 할당
+    hello[1] = 'e';
+    hello[2] = 'l';
+    hello[3] = 'l';
+    hello[4] = 'o';
+    hello[5] = '\0'; // NULL 종결 문자: 문자열의 끝 
+
+    printf("%s, %s!\n", hello, world);
+
+    return 0;
 }
--- Challenge/char_array_string.c
+++ Challenge/char_array_string.c
@@ -3,16 +3,17 @@
 
 int main(void)
 {
-    char str[80]; // 문자열 저장 공간 생성 
+    char hello[80]; // 문자열 저장 공간 생성
+    char world[40] = "World"; // 문자 배열 선언과 동시에 초기화 
 
-    str[0] = 'H'; // 문자 하나씩 할당 
-    str[1] = 'e';
-    str[2] = 'l';
-    str[3] = 'l';
-    str[4] = 'o';
-    str[5] = '\0'; // NULL 종결 문자
+    hello[0] = 'H'; // 문자 하나씩 할당
+    hello[1] = 'e';
+    hello[2] = 'l';
+    hello[3] = 'l';
+    hello[4] = 'o';
+    hello[5] = '\0'; // NULL 종결 문자: 문자열의 끝 
 
-    printf("%s\n", str);
+    printf("%s, %s!\n", hello, world);
 
-    return 0; 
+    return 0;
 }
