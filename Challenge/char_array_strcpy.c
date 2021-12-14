d6860b3ae92414f097a11ad23b11818afb71b4e7
Park YongJune <redplus@redplus.net>
2021-12-14 09:13:32 +0900

    C 언어 강의_strcpy() 함수로 문자열 리터럴을 문자 배열에 저장

--- Challenge/char_array_strcpy.c
+++ Challenge/char_array_strcpy.c
@@ -1,15 +1,16 @@
 // char_array_strcpy.c
 #define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
-#include <string.h>
+#include <string.h> // strcpy 사용을 위한 헤더 파일
 
 int main(void)
 {
     char buffer[80];
 
+    // strcpy() 함수로 문자열 리터럴을 문자 배열에 저장 
     strcpy(buffer, "C Language");
 
-    printf("%s\n", buffer);
+    printf("%s\n", buffer); // "C Language"
 
     return 0;
 }
