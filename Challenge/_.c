c345697549dd830cdf35dbdeb3255284ca3cdfb1
Park YongJune <redplus@redplus.net>
2021-12-17 13:35:12 +0900

    .

--- Challenge/_.c
+++ Challenge/_.c
@@ -1,15 +1,13 @@
-// text_center.c
-// 문자열 가운데 정렬시키기 
 #include <stdio.h>
 
 // 텍스트 가운데 정렬
 void text_center(int columns, char* text)
 {
     int min_width = strlen(text) + (columns - strlen(text)) / 2;
-    printf("%*s\n", min_width, text); // 최소 자리 만들고 문자열 채우기 
+    printf("%*s\n", min_width, text); // 최소 자리 만들고 문자열 채우기
 }
 
-// 텍스트 가운데 정렬
+// 텍트스 가운데 정렬: indent 구하고 putchar()함수로 들여쓰기 
 void text_align_center(int columns, char* text)
 {
     int indent = (columns - strlen(text)) / 2; // 왼쪽 여백 구하기 
@@ -26,5 +24,6 @@ int main(void)
     text_align_center(10, "Hello"); // __Hello___
     text_center(20, "Hello, World!"); // ___Hello, World!
     text_align_center(20, "Hello, World!"); // ___Hello, World!____
+
     return 0;
 }
