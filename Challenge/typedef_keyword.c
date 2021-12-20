baabdc0fbb16474fce27abf2a3e907b8cebd2f8c
Park YongJune <redplus@redplus.net>
2021-12-20 15:30:50 +0900

    typedef-keyword

--- Challenge/typedef_keyword.c
+++ Challenge/typedef_keyword.c
@@ -5,7 +5,7 @@
 typedef double number; // double을 number 이름으로도 사용
 
 int main(void)
-{    
+{
     number num1 = 1234.56; // double num1 = 1234.56; => 동일한 코드
 
     printf("%.2lf\n", num1); // 1234.56
