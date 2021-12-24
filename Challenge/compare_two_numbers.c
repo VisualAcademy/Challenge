f938cd3db042d72a04952c920c2bbacd6979d48e
Park YongJune <redplus@redplus.net>
2021-12-25 08:35:14 +0900

    C 언어 코드 샘플 - if 문 사용 두 수의 크기 비교

---
+++ Challenge/compare_two_numbers.c
@@ -0,0 +1,28 @@
+// C 언어 코드 샘플 - 두 수의 크기 비교 
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int num1 = 0;
+    int num2 = 0;
+    int _ = 0; // 버림(Discards)
+
+    printf("첫 번째 수: "); _ = scanf("%d", &num1);
+    printf("두 번째 수: "); _ = scanf("%d", &num2);
+
+    if (num1 > num2)
+    {
+        printf("%d이(가) %d보다 큽니다.\n", num1, num2);
+    }
+    if (num1 < num2)
+    {
+        printf("%d이(가) %d보다 작습니다.\n", num1, num2);
+    }
+    if (num1 == num2)
+    {
+        printf("%d이(가) %d와 같습니다.\n", num1, num2);
+    }
+
+    return 0;
+}
