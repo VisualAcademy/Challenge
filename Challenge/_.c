39e43e2718feb3c2211c68c0ebfca245858ce5d0
Park YongJune <redplus@redplus.net>
2021-12-26 02:19:36 +0900

    auto-scope

--- Challenge/_.c
+++ Challenge/_.c
@@ -1,30 +1,27 @@
-// static_shared.c
-// C 언어 코드 샘플 - 정적(공유) 변수 사용하기
+// auto_scope.c
+// C 언어 코드 샘플 - 자동 변수 사용하기
 #include <stdio.h>
 
-void func1(void);
-void func2(void);
-
-static int num = 200; // 모든 함수에서 공유(Shared)해서 사용 가능한 정적 변수 
+void sub(int num)
+{
+    printf("%d\n", num); 
+}
 
 int main(void)
 {
-    auto int num = 1234; // 지역 변수 num
-    func1(); // 20
-    func2(); // 210
-    printf("[3] main.num -> %d\n", num); // 1234
+    auto int num = 10; // 지역 변수 선언과 동시에 초기화
+    printf("%d\n", num); // 10
+    {
+        auto int num = 12; 
+        printf("%d\n", num); // 12
+        {
+            auto int num = 21;
+            printf("%d\n", num); // 21
+            sub(num); // 21
+        }    
+        sub(num); // 12
+    }
+    sub(num); // 10
 
     return 0;
-}
-
-void func1(void)
-{
-    auto int num = 20; // 지역 변수 num
-    printf("[1] fun1.num -> %d\n", num); // 20
-}
-
-void func2(void)
-{
-    num += 10; // 공유 변수 num
-    printf("[2] (global).num -> %d\n", num); // 210
 }
