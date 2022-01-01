bea58431ad34c9f533d7202f79e07c597210782e
Park YongJune <redplus@redplus.net>
2022-01-01 09:19:58 +0900

    Code Cleanup

--- Challenge/signed_integer.c
+++ Challenge/signed_integer.c
@@ -10,10 +10,10 @@ int main(void)
     long long ll = 9223372036854775807; // 64비트 부호 있는 정수형 변수 선언 및 할당
 
     //[2] 자리 표시자: short(%d), int(%d), long(%ld), long long(%lld) 
-    printf("short: %d\n", s);
-    printf("int: %d\n", i);
-    printf("long: %ld\n", l);
-    printf("long long: %lld\n", ll);
+    printf("short: %d\n", s); // short: 32767
+    printf("int: %d\n", i); // int: 2147483647
+    printf("long: %ld\n", l); // long: 2147483647
+    printf("long long: %lld\n", ll); // long long: 9223372036854775807
 
     return 0;
 }
