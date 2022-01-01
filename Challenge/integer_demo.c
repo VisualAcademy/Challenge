be9518679e56f98d45a39dddd576c856f8e3349f
Park YongJune <redplus@redplus.net>
2022-01-01 09:16:18 +0900

    Code Cleanup

--- Challenge/integer_demo.c
+++ Challenge/integer_demo.c
@@ -5,8 +5,8 @@ int main(void)
     int min = -2147483647 - 1; // 정수형이 가질 수 있는 가장 작은값
     int max = +2147483647; // 정수형이 가질 수 있는 가장 큰값
 
-    printf("int 변수의 최솟값: %d\n", min);
-    printf("int 변수의 최댓값: %d\n", max);
+    printf("int 변수의 최솟값: %d\n", min); // int 변수의 최솟값: -2147483648
+    printf("int 변수의 최댓값: %d\n", max); // int 변수의 최댓값: 2147483647
 
     return 0;
 }
