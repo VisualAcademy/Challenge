bf770b96d10cf65d340272800658dc226f3ee621
Park YongJune <redplus@redplus.net>
2022-01-11 04:29:33 +0900

    Code Cleanup

---
+++ Challenge/input_name.c
@@ -0,0 +1,19 @@
+// input_name.c
+// 예제: 콘솔로부터 이름을 입력받아 출력하는 프로그램   
+#define _CRT_SECURE_NO_WARNINGS // scanf 보안 경고로 인한 컴파일 에러 방지 
+#include <stdio.h>
+
+int main(void)
+{
+    char name[20]; // 문자의 배열 => 문자열을 담을 수 있는 그릇 
+    int _; // 버림
+
+    //[1] 입력
+    printf("이름을 입력하세요: ");
+    _ = scanf("%s", name); // 문자열은 %s 서식 지정자로 받기 
+
+    //[2] 출력
+    printf("안녕하세요. %s님.", name);
+
+    return 0;
+}
