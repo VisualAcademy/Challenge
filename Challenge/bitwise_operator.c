f2492e5e5dfdc5fbfd303347540e701adcc45c02
Park YongJune <redplus@redplus.net>
2022-01-14 04:51:38 +0900

    .

--- Challenge/bitwise_operator.c
+++ Challenge/bitwise_operator.c
@@ -8,10 +8,13 @@ int main(void)
 
     int _and = x & y; // 0010 => 2
     printf("%d\n", _and); // 2
+
     int _or = x | y; // 1110 => 14
     printf("%d\n", _or); // 14
+
     int _xor = x ^ y; // 1100 => 12
     printf("%d\n", _xor); // 12
+
     // 2의 보수법에 의해서 1010+1 그리고 부호를 -로 -1011 => -11
     int _not = ~x; // ~~~~0101 => -11
     printf("%d\n", _not); // -11
