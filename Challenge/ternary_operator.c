133eb6ad1db79b9a9993c253227b1efa51244390
Park YongJune <redplus@redplus.net>
2022-01-14 11:10:55 +0900

    .

--- Challenge/ternary_operator.c
+++ Challenge/ternary_operator.c
@@ -4,13 +4,13 @@
 
 int main(void)
 {
-    const char* result1 = 
+    const char* result1 =
         ('A' < 'B') ? "'A'는 'B'보다 작습니다." : "A B C 순서로 커집니다.";
-    const char* result2 = 
-        ('Z' < 'a') 
-        ? 
-        "'Z'는 'a'보다 작습니다." 
-        : 
+    const char* result2 =
+        ('Z' < 'a')
+        ?
+        "'Z'는 'a'보다 작습니다."
+        :
         "대문자보다 소문자가 더 큽니다.";
 
     printf("%s\n", result1); // 'A'는 'B'보다 작습니다.
