1ded9b8f1f70686f7a27a67b97f17a1aa5ab9f8b
Park YongJune <redplus@redplus.net>
2022-01-25 02:48:42 +0900

    .

--- Challenge/positive_negative_zero.c
+++ Challenge/positive_negative_zero.c
@@ -3,19 +3,19 @@
 
 int main(void)
 {
-    int number = -1234;
+    int number = -1234; //[1] 변수 선언 및 초기화 
 
     if (number > 0)
     {
-        printf("%d는 양수입니다.\n", number);
+        printf("%d는 양수입니다.\n", number); //[A] 양수일 때
     }
     else if (number < 0)
     {
-        printf("%d는 음수입니다.\n", number);
+        printf("%d는 음수입니다.\n", number); //[B] 음수일 때
     }
     else
     {
-        printf("%d는 0입니다.\n", number);
+        printf("%d는 0입니다.\n", number); //[C] 0일 때
     }
 
     return 0;
