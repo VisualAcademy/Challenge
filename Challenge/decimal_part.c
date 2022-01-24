96b737c5724e667b9ab65a4e7953a865af338085
Park YongJune <redplus@redplus.net>
2022-01-24 22:35:16 +0900

    .

--- Challenge/decimal_part.c
+++ Challenge/decimal_part.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-    float money = 88.99; 
+    float money = 88.99;
 
     printf("傈眉: %.2f\n", money); // 88.99
     printf("家荐何: %d\n", (int)((money - (int)money) * 100)); // 98
     printf("家荐何: %d\n", (int)((money - (int)money) * 1000000)); // 989997
-    
+
     return 0;
 }
