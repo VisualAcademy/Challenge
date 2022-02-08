647c3f532c2000075043593af82dee1d32ba1367
Park YongJune <redplus@redplus.net>
2022-02-09 04:15:59 +0900

    .

--- Challenge/_.c
+++ Challenge/_.c
@@ -1,16 +1,16 @@
-// input_unsigned_integer.c 
-//[?] 표준 입력인 콘솔로부터 나이를 정수로 입력 받아 출력하는 프로그램 
 #define _CRT_SECURE_NO_WARNINGS // scanf 보안 경고로 인한 컴파일 에러 방지 
 #include <stdio.h>
 
 int main(void)
 {
-    unsigned int age;
+    float num1;
+    double num2;
+    long double num3;
 
-    printf("나이를 입력하세요: "); // 프롬프트(Prompt, 안내 메시지)
-    scanf("%u", &age); // %u 서식 지정자로 부호없는 정수 입력 받기 
+    // 서식 지정자(float(%f), double(%lf), long double(%Lf)) 
+    scanf("%f %lf %Lf", &num1, &num2, &num3);
 
-    printf("나이: %d\n", age); // 입력된 값 출력 
+    printf("%.15f\n%.15lf\n%.15Lf\n", num1, num2, num3);
 
     return 0;
 }
