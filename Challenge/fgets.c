bd91893637bd72f2b4f79b113ffa442c41c59a97
Park YongJune <redplus@redplus.net>
2022-02-18 07:11:22 +0900

    .

--- Challenge/fgets.c
+++ Challenge/fgets.c
@@ -4,7 +4,7 @@ int main(void)
 {
     char name[20] = { NULL }; // 이름 저장 공간 준비
 
-    // 표준 입력으로부터 20자 입력받기: fgets()
+    // 표준 입력으로부터 입력받기: fgets()
     fgets(name, 20, stdin);
 
     printf("%s\n", name);
