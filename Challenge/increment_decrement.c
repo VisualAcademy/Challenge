5aa188b940ca7b3d7523850661cedb5049635ce1
Park YongJune <redplus@redplus.net>
2022-02-28 10:26:23 +0900

    .

--- Challenge/increment_decrement.c
+++ Challenge/increment_decrement.c
@@ -3,10 +3,10 @@
 int main(void)
 {
     int num1 = 10;
-    num1++; // 증가
+    num1++; // 증가, num1 += 1; 또는 num1 = num1 + 1;과 같음
 
     int num2 = 10;
-    num2--; // 감소
+    num2--; // 감소, num2 -= 1; 또는 num2 = num2 - 1;과 같음
 
     printf("%d\n", num1); // 11
     printf("%d\n", num2); // 9
