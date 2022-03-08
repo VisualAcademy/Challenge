fcf1a23bac66c79ae3ff357a64bd954c82a88daa
Park YongJune <redplus@redplus.net>
2022-03-08 09:08:58 +0900

    .

--- Challenge/sum100.c
+++ Challenge/sum100.c
@@ -3,17 +3,12 @@
 
 int main(void)
 {
-    //[1] Input(입력)
     int sum = 0;
 
-    //[2] Process(처리)
-    for (int i = 1; i <= 100; i++) // i가 1에서 100까지 100번 반복
-    {
-        sum += i; // sum = sum + i; // 누적(SUM)
-    }
+    for (int i = 1; i <= 100; i++) // i를 1에서 100까지 100번 반복
+        sum += i; // sum = sum + i; // 누적(SUM) 
 
-    //[3] Output(출력)
-    printf("1부터 100까지 정수의 합: %d\n", sum); // 1부터 100까지 정수의 합: 5050
+    printf("%d\n", sum); // 5050 
 
     return 0;
 }
