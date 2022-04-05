8198b983ce4610f2b5ad7aeec46eef874daee658
Park YongJune <redplus@redplus.net>
2022-04-05 17:02:28 +0900

    .

--- Challenge/leap_year.c
+++ Challenge/leap_year.c
@@ -17,7 +17,7 @@ int main(void)
     }
     else
     {
-        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+        if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
         {
             printf("true");
         }
