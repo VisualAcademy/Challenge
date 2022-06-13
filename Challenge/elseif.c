e689220ca30395b5f4759a8b13f0f846bfa292f4
Park YongJune <redplus@redplus.net>
2022-06-14 08:25:38 +0900

    else if 문 데모

--- Challenge/elseif.c
+++ Challenge/elseif.c
@@ -1,17 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS // scanf 보안 경고로 인한 컴파일 에러 방지 
 #include <stdio.h>
 
 int main(void)
 {
-    int num = 100;
+    int menu = 0;
+    int price = 0;
 
-    if (num == 100) // 조건 처리 1
+    printf("메뉴를 선택해주세요.\n");
+    printf("1. 아메리카노 2. 라떼 3. 모카\n");
+
+    scanf("%d", &menu);
+
+    if (menu == 1)
     {
-        printf("100입니다.\n");
+        price = 4100; // 아메리카노 선택
     }
-    else if (num == 200) // 조건 처리 2
+    else if (menu == 2)
     {
-        printf("200입니다.\n");
+        price = 4600; // 라떼 선택 
     }
+    else if (menu == 3)
+    {
+        price = 5100; // 모카 선택 
+    }
+    else
+    {
+        price = 0; // 없는 메뉴
+    }
+
+    printf("가격: %d\n", price);
 
     return 0;
 }
---
+++ Challenge/elseif_demo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num = 100;
+
+    if (num == 100) // 조건 처리 1
+    {
+        printf("100입니다.\n");
+    }
+    else if (num == 200) // 조건 처리 2
+    {
+        printf("200입니다.\n");
+    }
+
+    return 0;
+}
