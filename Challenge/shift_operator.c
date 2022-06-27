75548ce578d7df88d4af6c4c9a6046d164e0d54c
Park YongJune <redplus@redplus.net>
2022-06-27 09:43:57 +0900

    .

--- Challenge/shift_operator.c
+++ Challenge/shift_operator.c
@@ -3,10 +3,10 @@
 
 int main(void)
 {
-    int num = 2; // 0010 
+    int num = 2; // 0010, 2 
 
-    int left = num << 1; // 0010 -> 0100: 4
-    int right = num >> 1; // 0010 -> 0001: 1
+    int left = num << 1; // 0010, 2 -> 0100, 4
+    int right = num >> 1; // 0010, 2 -> 0001, 1
 
     printf("%d\n", left); // 4
     printf("%d\n", right); // 1
