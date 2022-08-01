2e899ef7f36e034abd246f44f6e103e48c8b1a9d
Park YongJune <redplus@redplus.net>
2022-08-02 05:44:15 +0900

    반복문 없이 만드는 반복

---
+++ Challenge/goto_loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 0; // 초기식
+    loop_body:
+        printf("C Language\n"); // 실행문
+        i++; // 변화식
+    if (i < 3) // 조건식 
+    {
+        goto loop_body; 
+    }
+
+    return 0; 
+}
