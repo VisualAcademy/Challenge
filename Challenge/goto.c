b2475db434582b40bae40604acbe6ec595d79708
Park YongJune <redplus@redplus.net>
2022-09-13 04:16:19 +0900

    .

--- Challenge/goto.c
+++ Challenge/goto.c
@@ -1,38 +1,16 @@
-// goto문: 지정한 레이블(책갈피)로 이동하는 구문
-#define _CRT_SECURE_NO_WARNINGS // scanf() 보안 경고에 따른 컴파일 에러 방지
 #include <stdio.h>
 
 int main(void)
 {
-    int chapter = 0;
-
-    printf("시작\n");
-
-Start:
-    printf("0, 1, 2 중 하나 입력: _\b");
-    scanf("%d", &chapter);
-
-    if (chapter == 1)
+    for (int i = 0; i < 10; i++)
     {
-        goto Chapter1;
-    }
-    else if (chapter == 2)
-    {
-        goto Chapter2;
-    }
-    else
-    {
-        goto End;
+        printf("%d\n", i);  // 한 번만 실행된다
+        goto exit;  // exit 레이블로 이동
     }
 
-Chapter1:
-    printf("1장입니다.\n");
-Chapter2:
-    printf("2장입니다.\n");
-    goto Start;
+    printf("Hello World!\n");  // 실행되지 않는다
 
-End:
-    printf("종료\n");
-
+exit:
+    printf("exit\n");
     return 0;
 }
