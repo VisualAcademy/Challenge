d70f4ec7811c85d18dc89872ce5e091af7b11b07
Park YongJune <redplus@redplus.net>
2022-09-15 08:44:15 +0900

    .

--- Challenge/timestable.c
+++ Challenge/timestable.c
@@ -3,19 +3,13 @@
 
 int main(void)
 {
-    for (int i = 2; i <= 9; i++)
+    for (int i = 1; i <= 9; i++) // 세로 반복: 구구단 세로 출력
     {
-        printf("%4d단  ", i);
-    }
-    printf("\n");
-
-    for (int i = 1; i <= 9; i++) // 행 반복: 구구단 세로 출력
-    {
-        for (int j = 2; j <= 9; j++) // 열 반복: 구구단 가로 출력
+        for (int j = 2; j <= 9; j++) // 가로 반복: 구구단 가로 출력
         {
-            printf("%d*%d=%2d  ", j, i, j * i); // i와 j의 위치를 바꿔서 출력
+            printf("%d*%d=%2d ", j, i, j * i); // i와 j의 위치를 바꿔서 출력
         }
-        printf("\n"); // 한 줄 출력 후 줄바꿈
+        printf("\n"); // 구구단 1줄 출력 후 줄바꿈
     }
 
     return 0;
