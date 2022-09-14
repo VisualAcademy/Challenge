0d8c39d6c1e9a96ed85fdb0655b53830f2539d69
Park YongJune <redplus@redplus.net>
2022-09-15 06:43:59 +0900

    .

--- Challenge/goto_loop_docs.c
+++ Challenge/goto_loop_docs.c
@@ -3,13 +3,13 @@
 int main(void)
 {
     int i = 0; // 초기식
-loop_body:
-    printf("%d\n", i); // 실행문
-    i++; // 변화식
+    loop_body:
+        printf("C Language\n"); // 실행문
+        i++; // 변화식
     if (i < 3) // 조건식 
     {
         goto loop_body; // loop_body 레이블로 이동
     }
 
-    return 0;
+    return 0; 
 }
