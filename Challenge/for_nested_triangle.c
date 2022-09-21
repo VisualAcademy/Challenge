dc740d1b09de7d1493ba26786d052bfa2c0d1930
Park YongJune <redplus@redplus.net>
2022-09-22 04:55:54 +0900

    .

--- Challenge/for_nested_triangle.c
+++ Challenge/for_nested_triangle.c
@@ -3,11 +3,12 @@
 int main(void)
 {
     // for 중첩 루프로 (5 * 5)번 반복하여 직삼각형 모양 출력
-    for (int i = 0; i < 5; i++) // 높이(height) 반복
+    for (int height = 0; height < 5; height++) // 높이(height) 반복
     {
-        for (int j = 0; j < 5; j++) // 너비(width) 반복
+        for (int width = 0; width < 5; width++) // 너비(width) 반복
         {
-            if (j <= i) // 별표 출력 조건 처리: i 값만큼 j를 반복하면 하나씩 증가됨
+            // 별표 출력 조건 처리: height 값만큼 width를 반복하면 하나씩 증가됨
+            if (width <= height)
             {
                 printf("*"); // 별표 출력
             }
