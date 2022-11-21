310cc767cf16b00a99c1f9ffbefd1ab9ef0f54ee
Park YongJune <redplus@redplus.net>
2022-11-22 07:57:34 +0900

    memory_allocation

--- Challenge/memory_allocation.c
+++ Challenge/memory_allocation.c
@@ -1,7 +1,7 @@
 // memory_allocation.c
 // 동적 메모리 할당
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> // malloc 함수 사용을 위한 헤더 파일 추가 
 
 int main(void)
 {
@@ -9,7 +9,7 @@ int main(void)
     int* num;
 
     //[2] 메모리 할당
-    num = malloc(sizeof(int));
+    num = (int*)malloc(sizeof(int)); // 반드시 sizeof 연산자로 크기 지정
 
     if (num) // 유효성 검사: NULL이 아니면 num 변수 사용
     {
@@ -17,7 +17,7 @@ int main(void)
         *num = 1234;
 
         //[4] 포인터 변수의 값 참조
-        printf("%d\n", *num);
+        printf("%d\n", *num); // 1234 
 
         //[5] 메모리 해제
         free(num);
