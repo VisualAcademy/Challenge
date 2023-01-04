1639185a8742aa0ef8c599d36db92a27d2ddcd67
Park YongJune <redplus@redplus.net>
2023-01-04 22:44:59 +0900

    .

--- Challenge/_.c
+++ Challenge/_.c
@@ -1,19 +1,15 @@
-// 비트 연산자: &, |, ~, ^
 #include <stdio.h>
 
 int main(void)
 {
-    int x = 10; // 1010
-    int y = 6; // 0110
-
-    printf("%d\n", x & y); // 2
-
-    printf("%d\n", x | y); // 14
-
-    printf("%d\n", x ^ y); // 12
-
-    // 2의 보수법에 의해서 1010+1 그리고 부호를 -로 -1011 => -11
-    printf("%d\n", ~x); // -11
+    int i = 0; // 초기식
+loop_body:
+    printf("C Language\n"); // 실행문
+    i++; // 변화식
+    if (i < 3) // 조건식 
+    {
+        goto loop_body; // loop_body 레이블로 이동
+    }
 
     return 0;
 }
