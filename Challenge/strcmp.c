157cef1637fa40c4d3baf14a7ff0e425479850a9
Park YongJune <redplus@redplus.net>
2023-03-31 20:45:55 +0900

    .

--- Challenge/strcmp.c
+++ Challenge/strcmp.c
@@ -4,7 +4,7 @@
 int main(void)
 {
     printf("AAA < BBB: %d\n", strcmp("AAA", "BBB")); // -1
-    printf("AAA = AAA: %d\n", strcmp("AAA", "AAA")); // 0
+    printf("AAA == AAA: %d\n", strcmp("AAA", "AAA")); // 0
     printf("ZZZ > AAA: %d\n", strcmp("ZZZ", "AAA")); // 1
 
     return 0;
