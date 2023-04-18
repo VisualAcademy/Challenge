7fee4f222004c9e0907f3f0d960e3fb8d5f17eb2
Park YongJune <redplus@redplus.net>
2023-04-19 00:16:49 +0900

    tolower.c

---
+++ Gilbut/tolower.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char ch;
+
+    printf("input: ");
+    ch = getchar(); // 'A'
+
+    // 대문자에 32를 더해서 소문자로 변경
+    ch = ch + 32; // 'A' + 32 => 소문자 'a'
+    printf("output: %c", ch);
+
+    return 0;
+}
