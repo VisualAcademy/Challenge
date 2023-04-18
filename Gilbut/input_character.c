ad4d61d8377dbdb3b2dd9ba3f928cf47be102037
Park YongJune <redplus@redplus.net>
2023-04-19 00:05:45 +0900

    input_character.c

---
+++ Gilbut/input_character.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS // scanf 보안 경고로 인한 컴파일 에러 방지 
+#include <stdio.h>
+
+int main(void)
+{
+    char c;
+
+    // 문자 하나를 입력 후 엔터키를 누릅니다.
+    scanf("%c", &c);
+
+    // 대문자 A를 입력했다면 %c는 문자 A, %d는 정수 65를 출력합니다.
+    printf("%c: %d\n", c, c);   // A: 65
+
+    return 0;
+}
